19ea85b1f96dd0940c4d77bac2c34602ab2c15bc
jens-vervaeren <vervaeren.jens@gmail.com>
2026-01-18 15:14:16 +0100

    install of radix-ui

--- src/app/routes/quests/getQuests.route.tsx
+++ src/app/routes/quests/getQuests.route.tsx
@@ -1,8 +1,14 @@
 import { createRoute } from "@tanstack/react-router"
+import { Flex, Text, Button } from "@radix-ui/themes"
 import { rootRoute } from "../root.route"
 
 export const getQuestsRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: '/quests',
-  component: () => <div>Quests</div>,
+  component: () => (
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button>Let's go</Button>
+    </Flex>
+  ),
 })
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
+import "@radix-ui/themes/styles.css";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { startMockServer } from "./app/server"
 import { RouterProvider } from "@tanstack/react-router"
 import { router } from './app/routes/index.ts'
+import { Theme } from '@radix-ui/themes'
 
 startMockServer()
 
-createRoot(document.getElementById('root')!).render(
+const appRoot = createRoot(document.getElementById('root')!)
+
+appRoot.render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Theme appearance="dark" accentColor="violet">
+      <RouterProvider router={router} />
+    </Theme>
   </StrictMode>,
 )
